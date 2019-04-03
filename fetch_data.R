54145ba9c8ea60dfd623c94ab8b62173245eb3b5
Khushbu Patel <kpatel427@gatech.edu>
2019-04-03 07:59:18 -0400

    update

--- fetch_data.R
+++ fetch_data.R
@@ -1,5 +1,5 @@
 # download GEO data
-.libPaths("/mnt/isilon/maris_lab/target_nbl_ngs/KP/RShiny/packages/")
+.libPaths("~/KP/RShiny/packages/")
 
 library(GEOquery)
 library(xml2)
@@ -7,7 +7,7 @@ library(R.utils)
 
 
 
-#open file with gse_accessions.txt
+#open file with gse_accessions.txt(a text file containing all GSExxxxx accessions)
 con = file("gse_accessions_filtered.txt", "r")
 
 while ( TRUE ) {
@@ -16,13 +16,13 @@ while ( TRUE ) {
     break
   }
   print(line)
-  geo_id <- toupper(line)
-  if(file.exists(paste0("/mnt/isilon/maris_lab/target_nbl_ngs/KP/RShiny/GEOdata/",geo_id))){
+  geo_id <- toupper(line) # converting lower case letters to upper case
+  if(file.exists(paste0("~/KP/RShiny/GEOdata/",geo_id))){
     print("Directory exists!") # repetitive GSE ID; skip to next one
   }
   else {
-  dir.create(file.path("/mnt/isilon/maris_lab/target_nbl_ngs/KP/RShiny/GEOdata/", geo_id)) # create directory
-  gse <- getGEO(geo_id, GSEMatrix=TRUE, destdir = paste0("/mnt/isilon/maris_lab/target_nbl_ngs/KP/RShiny/GEOdata/",geo_id) )
+  dir.create(file.path("~/KP/RShiny/GEOdata/", geo_id)) # create directory
+  gse <- getGEO(geo_id, GSEMatrix=TRUE, destdir = paste0("~/KP/RShiny/GEOdata/",geo_id) )
   print(paste0("Data download finished for ",geo_id))
   }
 
